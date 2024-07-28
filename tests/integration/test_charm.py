6fa1953f8799a8a4452d85b7f3d25417882b6ab5
guillaume <guillaume.belanger27@gmail.com>
2024-07-28 13:13:38 -0400

    chore: add integration test
    Signed-off-by: guillaume <guillaume.belanger27@gmail.com>

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -2,7 +2,6 @@
 # Copyright 2024 Guillaume Belanger
 # See LICENSE file for licensing details.
 
-import asyncio
 import logging
 from pathlib import Path
 
@@ -14,6 +13,8 @@ logger = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./charmcraft.yaml").read_text())
 APP_NAME = METADATA["name"]
+DB_CHARM_NAME = "mongodb-k8s"
+DB_CHARM_CHANNEL = "6/beta"
 
 
 @pytest.mark.abort_on_fail
@@ -26,11 +27,17 @@ async def test_build_and_deploy(ops_test: OpsTest, request):
     charm_path = Path(request.config.getoption("--charm_path")).resolve()
     resources = {"ella-image": METADATA["resources"]["ella-image"]["upstream-source"]}
 
-    await asyncio.gather(
-        ops_test.model.deploy(
-            entity_url=charm_path, resources=resources, application_name=APP_NAME
-        ),
-        ops_test.model.wait_for_idle(
-            apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
-        ),
+    await ops_test.model.deploy(
+        entity_url=charm_path, resources=resources, application_name=APP_NAME
+    )
+    await ops_test.model.deploy(
+        DB_CHARM_NAME,
+        application_name=DB_CHARM_NAME,
+        channel=DB_CHARM_CHANNEL,
+    )
+
+    await ops_test.model.integrate(relation1=f"{APP_NAME}:database", relation2=f"{DB_CHARM_NAME}")
+
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
     )
