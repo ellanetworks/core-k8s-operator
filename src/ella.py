b15fea68b6d0a6b1eef4cfb1878454ea0406cc45
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-08-04 22:15:14 -0400

    chore: add 5g gnb identity integration in integration tests (#21)
    Signed-off-by: guillaume <guillaume.belanger27@gmail.com>

--- src/ella.py
+++ src/ella.py
@@ -48,7 +48,7 @@ class Ella:
             gnb (GnodeB): The GnodeB to add to the inventory.
         """
         inventory_url = f"{self.url}/{GNB_CONFIG_URL}/{gnb.name}"
-        data = {"tac": gnb.tac}
+        data = {"tac": str(gnb.tac)}
         self._add_resource_to_inventory(inventory_url, gnb.name, data)
 
     def delete_gnb_from_inventory(self, gnb: GnodeB) -> None:
@@ -72,17 +72,17 @@ class Ella:
         return resources
 
     def _add_resource_to_inventory(self, url: str, resource_name: str, data: dict) -> None:
+        response = requests.post(url, headers=JSON_HEADER, json=data)
         try:
-            response = requests.post(url, headers=JSON_HEADER, json=data)
             response.raise_for_status()
-            logger.info("%s added to ella", resource_name)
         except requests.HTTPError as e:
-            logger.error("Failed to add %s to ella: %s", resource_name, e)
+            logger.error("Failed to add %s to ella: %s. %s", resource_name, e, e.response.text)
+        logger.info("%s added to ella", resource_name)
 
     def _delete_resource_from_inventory(self, inventory_url: str, resource_name: str) -> None:
+        response = requests.delete(inventory_url)
         try:
-            response = requests.delete(inventory_url)
             response.raise_for_status()
-            logger.info(f"{resource_name} removed from webui")
         except requests.HTTPError as e:
-            logger.error(f"Failed to remove {resource_name} from webui: {e}")
+            logger.error("Failed to remove %s from webui: %s", resource_name, e)
+        logger.info("%s removed from webui", resource_name)
