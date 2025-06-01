e9040770d1b68bfea8005be6ef0a6ae46efb2871
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-06-01 10:56:20 -0400

    chore: move logic to go code
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -1,8 +1,10 @@
 package integration_test
 
 import (
+	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/ellanetworks/core-k8s/integration/juju"
 )
@@ -10,30 +12,31 @@ import (
 const (
 	CharmPath       = "../ella-core-k8s_amd64.charm"
 	JujuModelName   = "test-model"
+	CloudName       = "test-cloud"
 	ApplicationName = "ella-core"
 	EllaCoreImage   = "ghcr.io/ellanetworks/ella-core:v0.0.15"
 )
 
-// func waitForActiveStatus(appName string, client *juju.Juju, timeout time.Duration) error {
-// 	start := time.Now()
+func waitForActiveStatus(appName string, client *juju.Juju, timeout time.Duration) error {
+	start := time.Now()
 
-// 	for {
-// 		if time.Since(start) > timeout {
-// 			return fmt.Errorf("timed out waiting for active status")
-// 		}
+	for {
+		if time.Since(start) > timeout {
+			return fmt.Errorf("timed out waiting for active status")
+		}
 
-// 		status, err := client.Status()
-// 		if err != nil {
-// 			return err
-// 		}
+		status, err := client.Status()
+		if err != nil {
+			return err
+		}
 
-// 		if status.Applications[appName].ApplicationStatus.Current == "active" {
-// 			return nil
-// 		}
+		if status.Applications[appName].ApplicationStatus.Current == "active" {
+			return nil
+		}
 
-// 		time.Sleep(1 * time.Second)
-// 	}
-// }
+		time.Sleep(1 * time.Second)
+	}
+}
 
 func TestIntegration(t *testing.T) {
 	if os.Getenv("INTEGRATION") == "" {
@@ -42,7 +45,24 @@ func TestIntegration(t *testing.T) {
 
 	jujuClient := juju.New()
 
-	err := jujuClient.AddModel(&juju.AddModelOptions{
+	err := jujuClient.AddK8s(&juju.AddK8sOptions{
+		Name:   CloudName,
+		Client: true,
+	})
+	if err != nil {
+		t.Fatalf("Failed to add k8s: %v", err)
+	}
+
+	t.Log("K8s cloud is added")
+
+	err = jujuClient.Bootstrap(&juju.BootstrapOptions{
+		CloudName: CloudName,
+	})
+	if err != nil {
+		t.Fatalf("Failed to bootstrap: %v", err)
+	}
+
+	err = jujuClient.AddModel(&juju.AddModelOptions{
 		Name: JujuModelName,
 	})
 	if err != nil {
@@ -51,23 +71,23 @@ func TestIntegration(t *testing.T) {
 
 	t.Log("Model is added")
 
-	// err = jujuClient.Deploy(&juju.DeployOptions{
-	// 	Charm:           CharmPath,
-	// 	ApplicationName: ApplicationName,
-	// 	Resources: map[string]string{
-	// 		"core-image": EllaCoreImage,
-	// 	},
-	// })
-	// if err != nil {
-	// 	t.Fatalf("Failed to deploy: %v", err)
-	// }
+	err = jujuClient.Deploy(&juju.DeployOptions{
+		Charm:           CharmPath,
+		ApplicationName: ApplicationName,
+		Resources: map[string]string{
+			"core-image": EllaCoreImage,
+		},
+	})
+	if err != nil {
+		t.Fatalf("Failed to deploy: %v", err)
+	}
 
-	// t.Log("Charm is deployed")
+	t.Log("Charm is deployed")
 
-	// err = waitForActiveStatus(ApplicationName, jujuClient, 5*time.Minute)
-	// if err != nil {
-	// 	t.Fatalf("Failed to wait for active status: %v", err)
-	// }
+	err = waitForActiveStatus(ApplicationName, jujuClient, 5*time.Minute)
+	if err != nil {
+		t.Fatalf("Failed to wait for active status: %v", err)
+	}
 
-	// t.Log("Charm is active")
+	t.Log("Charm is active")
 }
